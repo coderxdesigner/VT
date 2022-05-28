05bc40cd007df8d4b85c428daeac89c3c054383b
Rachel <Rachel.Heneault@gmail.com>
2022-05-28 06:38:01 -0400

    css layout for custom radio display

--- assets/scripts.js
+++
@@ -1,9 +0,0 @@
-;(function () {
-  'use strict'
-  const inputs = document.getElementsByTagName('input')
-  inputs.addEventListener('onChange', changeInput)
-
-  const changeInput = () => {
-    console.log(this)
-  }
-})()
